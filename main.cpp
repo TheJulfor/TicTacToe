2edd8074efe3fff245d8e265be61b18218e1f0a3
TheJulfor <romagordzilevsky@gmail.com>
2026-01-08 18:11:57 +0500

    Add main menu

--- main.cpp
+++ main.cpp
@@ -5,7 +5,25 @@
 int main() {
     system("chcp 65001 > nul");
 
-    Game game;
-    initGame(game);
-    gameLoop(game);
+    std::string input;
+    while (input != "0") {
+        std::cout << "==== Крестики-нолики ====\n";
+        std::cout << "1. Начать новую игру\n";
+        std::cout << "=========================\n";
+        std::cout << "0. Выход\n";
+        std::cout << "\n";
+        std::cout << "Ваш выбор:";
+
+        std::getline(std::cin, input);
+        if (input == "1") {
+            Game game;
+            std::cout << "Начинаем новую игру...\n\n\n";
+            initGame(game);
+            gameLoop(game);
+        } else if (input == "0") {
+            std::cout << "Выход...";
+        } else {
+            std::cout << "Неверный ввод, попробуйте снова..\n\n";
+        }
+    }
 }
